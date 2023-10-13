3069ebda335779d21863cede90b9983baad2ca54
Yuri Souza <yurdesou@gmail.com>
2023-10-13 19:14:07 -0300

    feaat: Update theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
 final ThemeData theme = ThemeData(
+  useMaterial3: true,
   fontFamily: 'JetBrains Mono Regular',
-  iconTheme: IconThemeData(color: Colors.blueAccent)
+  iconTheme: const IconThemeData(color: Colors.blueAccent),
+  cardTheme: const CardTheme(
+    elevation: 4,
+    color: Colors.white,
+    surfaceTintColor: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+  ),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    elevation: 4,
+    backgroundColor: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+    ),
+  ),
 );
