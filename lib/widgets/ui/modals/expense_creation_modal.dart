f1304604e6b91a212495a59e22717cd320d48d57
Yuri Souza <yurdesou@gmail.com>
2023-10-19 22:39:32 -0300

    feat: Implement category selection on expense creation

--- lib/widgets/ui/modals/expense_creation_modal.dart
+++ lib/widgets/ui/modals/expense_creation_modal.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_expenses_tracker/models/expense.dart';
 import 'package:flutter_expenses_tracker/widgets/ui/generic/input.dart';
 import 'package:flutter_expenses_tracker/widgets/ui/generic/date_input.dart';
+import 'package:flutter_expenses_tracker/widgets/ui/specialized/category_selector/category_selector.dart';
 
 class ExpenseCreationModal extends StatefulWidget {
   const ExpenseCreationModal({
@@ -22,10 +23,9 @@ class _ExpenseCreationModalState extends State<ExpenseCreationModal> {
   final _formValues = {
     "title": "",
     "amount": 0,
-    "category": Category.food,
+    "category": null,
     "date": null
   };
-  String? _dateError;
 
   void _updateField(String fieldToUpdate, value) {
     setState(() {
@@ -33,23 +33,7 @@ class _ExpenseCreationModalState extends State<ExpenseCreationModal> {
     });
   }
 
-  void _updateDate(DateTime date) {
-    setState(() {
-      _formValues['date'] = date;
-
-      if (_dateError != null) {
-        _dateError = null;
-      }
-    });
-  }
-
   void _submitForm() {
-    if (_formValues['date'] == null) {
-      setState(() {
-        _dateError = 'Please insert a date.';
-      });
-    }
-
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       Expense expense = Expense(
@@ -67,85 +51,100 @@ class _ExpenseCreationModalState extends State<ExpenseCreationModal> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      // This handle the positioning of the modal when the keyboard is visible
-      padding: EdgeInsets.only(
-        bottom: MediaQuery.of(context).viewInsets.bottom,
-      ),
+    return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Input(
-                labelText: 'Title',
-                hintText: 'Groceries, PS5, etc...',
-                onSaved: (value) {
-                  _updateField('title', value);
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please insert a title.';
-                  }
+        // This handle the positioning of the modal when the keyboard is visible
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Input(
+                  labelText: 'Title',
+                  hintText: 'Groceries, PS5, etc...',
+                  onSaved: (value) {
+                    _updateField('title', value);
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please insert a title.';
+                    }
 
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Input(
-                      type: TextInputType.number,
-                      labelText: 'Amount',
-                      hintText: '0.00',
-                      prefixText: '\$',
-                      onSaved: (value) {
-                        _updateField('amount', value);
-                      },
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please insert an amount.';
-                        }
-
-                        return null;
-                      },
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: DateInput(
-                      labelText: 'Date',
-                      onSaved: (value) {
-                        _updateField('date', value);
-                      },
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please insert a date.';
-                        }
-
-                        return null;
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 64,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: FilledButton(
-                  onPressed: _submitForm,
-                  child: const Text('Create expense'),
+                    return null;
+                  },
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Input(
+                        type: TextInputType.number,
+                        labelText: 'Amount',
+                        hintText: '0.00',
+                        prefixText: '\$',
+                        onSaved: (value) {
+                          _updateField('amount', value);
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please insert an amount.';
+                          }
+
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: DateInput(
+                        labelText: 'Date',
+                        onSaved: (dateValue) {
+                          if (dateValue != null) {
+                            _updateField('date', dateValue);
+                          }
+                        },
+                        validator: (formattedValue) {
+                          if (formattedValue!.isEmpty) {
+                            return 'Please pick a date.';
+                          }
+
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                CategorySelector(
+                  onSaved: (category) {
+                    if (category != null) {
+                      _updateField('category', category);
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 64,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                    onPressed: _submitForm,
+                    child: const Text(
+                      'Create Expense',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
